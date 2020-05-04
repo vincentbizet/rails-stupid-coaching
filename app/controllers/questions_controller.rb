a53af42085c1c55561af172ba5113d07cee10097
vincentbizet <bizetvincent78@gmail.com>
2020-05-04 22:04:24 +0200

    start exo 2 rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    # @questions = ['I am going to work', '?']
+    #@answer = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+    @question = params[:question]
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include?('?')
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
